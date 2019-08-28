30655e105ef07baef2945b6ecfe2237d39a62e5b
jiko797torayo <oec7dwayne7kj@gmail.com>
2019-08-28 22:11:07 +0900

    connectでstateとactionsとの関連付けを行う

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,23 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { increment, decrement } from '../actions'
 
-const App = () => <Counter></Counter>
-
-class Counter extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { count: 0 }
-  }
-
-  handlePlusButton = () => {
-    this.setState({ count: this.state.count + 1 })
-  }
-
-  handleMinusButton = () => {
-    this.setState({ count: this.state.count - 1 })
-  }
-
+class App extends Component {
   render() {
+    const props = this.props
     return (
       <React.Fragment>
-        <div>count: { this.state.count }</div>
-        <button onClick={this.handlePlusButton}>+1</button>
-        <button onClick={this.handleMinusButton}>-1</button>
+        <div>value: { props.value }</div>
+        <button onClick={props.increment}>+1</button>
+        <button onClick={props.decrement}>-1</button>
       </React.Fragment>
     )
   }
 }
 
-export default App;
+// 子コンポーネントでStateの情報を使いたいとき、本来なら親コンポーネントからPropsとして引き継いでもらわないといけないんですけど、「mapStateToProps」を使うとそんなことはお構いなしに親コンポーネントから引き継がなくても子コンポーネントからすぐに使うことができるようになります。
+const mapStateToProps = state => ({ value: state.count.value })
+// この「mapDispatchToProps」を使うとこれも親や子なんかお構いなし！孫コンポーネントからActionを即呼びできるようになります。
+const mapDispatchToProps = ({ increment, decrement })
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
