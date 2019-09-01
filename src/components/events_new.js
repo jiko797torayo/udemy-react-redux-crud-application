2577b2597d8094c9b5f622486386b97d80011a87
jiko797torayo <oec7dwayne7kj@gmail.com>
2019-09-01 21:39:52 +0900

    イベント新規作成画面のコンポーネントをreduxFormでdecorateする

---
+++ src/components/events_new.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+// import { postEvent } from '../actions'
+import { Link } from 'react-router-dom'
+
+class EventsNew extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <div>new</div>
+      </React.Fragment>
+    )
+  }
+}
+
+// const mapDispatchToProps = ({ postEvents })
+
+export default connect(null, null)(EventsNew)
