b3b724b30da3636121b230a49194e8b4a0219cad
jiko797torayo <oec7dwayne7kj@gmail.com>
2019-09-01 16:03:04 +0900

    イベント一覧を画面に表示する

--- src/index.js
+++ src/index.js
@@ -2,23 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // storeを提供する
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 
 // 作成したstoreを全コンポーネントにわたすproviderを提供する
 import { Provider } from 'react-redux'
 
+// 非同期処理を可能にするためのライブラリ
+// actionの代わりに関数を返すことができる
+import thunk from 'redux-thunk'
+
 import './index.css';
 import reducer from './reducers'
 
-import App from './components/App';
+import EventsIndex from './components/events_index';
 import * as serviceWorker from './serviceWorker';
 
-const store = createStore(reducer)
+const store = createStore(reducer, applyMiddleware(thunk))
 
 // Reactのstoreのバケツリレーをなくすためのコード
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <EventsIndex />
   </Provider>,
   document.getElementById('root')
 );
