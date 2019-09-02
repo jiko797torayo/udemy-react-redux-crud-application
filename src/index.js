cdd22ee7158d06038bfa8f2d2fcd860853016662
jiko797torayo <oec7dwayne7kj@gmail.com>
2019-09-02 22:16:53 +0900

    イベント削除機能を実装する

--- src/index.js
+++ src/index.js
@@ -14,22 +14,31 @@ import thunk from 'redux-thunk'
 // リンク用パッケージ
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
+// デバッグがしやすくなるライブラリ
+import { composeWithDevTools } from 'redux-devtools-extension'
+
 import './index.css';
 import reducer from './reducers'
 
 import EventsIndex from './components/events_index';
 import EventsNew from './components/events_new';
+import EventsShow from './components/events_show';
 import * as serviceWorker from './serviceWorker';
 
-const store = createStore(reducer, applyMiddleware(thunk))
+// デバッグがしやすくするためのコード
+const enhancer = process.env.NODE_ENV === 'development' ?
+  composeWithDevTools(applyMiddleware(thunk)) : applyMiddleware(thunk)
+const store = createStore(reducer, enhancer)
 
 // Reactのstoreのバケツリレーをなくすためのコード
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
       <Switch>
-        <Route exact path="/events/new" component={EventsNew} />
+        <Route path="/events/new" component={EventsNew} />
+        <Route path="/events/:id" component={EventsShow} />
         <Route exact path="/" component={EventsIndex} />
+        <Route exact path="/events" component={EventsIndex} />
       </Switch>
     </BrowserRouter>
   </Provider>,
