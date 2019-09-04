99f59a517477d294e94648b932eb27969eb66ca7
jiko797torayo <oec7dwayne7kj@gmail.com>
2019-09-04 21:48:40 +0900

    イベント更新機能を実装する

--- src/reducers/events.js
+++ src/reducers/events.js
@@ -2,8 +2,10 @@
 import _ from 'lodash'
 
 import {
+  CREATE_EVENT,
   READ_EVENTS,
   READ_EVENT,
+  UPDATE_EVENT,
   DELETE_EVENT,
 } from '../actions'
 
@@ -11,6 +13,8 @@ import {
 export default (events = {}, action) => {
   switch (action.type) {
     case READ_EVENT:
+    case CREATE_EVENT:
+    case UPDATE_EVENT:
       const data = action.response.data
       return { ...events, [data.id]: data }
     case READ_EVENTS:
